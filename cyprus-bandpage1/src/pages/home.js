2a9e1af44695d7a72eb9853a5a98505ba469034a
Ben Cummings <bncmmngs@aol.com>
2021-03-02 14:16:49 -0500

    inital commit

--- cyprus-bandpage1/src/pages/home.js
+++ cyprus-bandpage1/src/pages/home.js
@@ -1,1 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Container, Row, Col} from 'react-bootstrap';
+
+const style = {
+    background: 'rgba(0,0,0,0,5)'
+}
+
+export default class Home extends React.Component {
+    render() {
+        return (
+
+            <Container id="about">
+                <h1>Cyprus</h1>
+
+                <Row className="show-grid" style={style}>
+                    <Col xs={12} sm={6}>
+                        <h3>
+                            
+                        </h3>
+
+
+
+
+                    </Col>
+
+
+
+
+                </Row>
+
+
+
+            </Container>
+
+
+        )
+    }
+}
